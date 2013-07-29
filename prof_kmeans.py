b7a1bd6f251a8f8a1af6427eb6ac20c00477b21b
Philippe Gervais <philippe.gervais@inria.fr>
2013-07-29 15:33:56 +0200

    Graph of exec. times for pairwise_distances_argmin

--- prof_kmeans.py
+++ prof_kmeans.py
@@ -1,8 +1,8 @@
 import utils
 
 import numpy as np
-from scipy.cluster.vq import kmeans, kmeans2
-from sklearn.cluster import k_means, MiniBatchKMeans
+from scipy.cluster.vq import kmeans
+from sklearn.cluster import k_means
 from sklearn.cluster.k_means_ import _labels_inertia
 from sklearn.datasets import make_blobs
 
@@ -13,44 +13,42 @@ from sklearn.datasets import make_blobs
 # are equal to one.
 n_clusters = 10
 
-np.random.seed(0)
+random_state = 4
 
 n_samples, n_features = 100000, 200
+#n_samples, n_features = 200, 10000
 X = make_blobs(n_samples, n_features, centers=n_clusters, random_state=0)[0]
 
-## n_samples, n_features = 10000, 200    # scipy faster
-## n_samples, n_features = 200, 10000    # scipy faster
-## n_samples, n_features = 200, 100000   # scipy faster, minibatch very long
+## n_samples, n_features = 10000, 200
+## n_samples, n_features = 200, 10000
+## n_samples, n_features = 200, 100000
 ## X = np.random.normal(size=(n_samples, n_features))
 
 tol = 1e-4
 
-print("\n-- scipy.cluster.vq")
-ratio = 1.
-np.random.seed(1)
-sc, _ = utils.timeit(profile(kmeans))(X, n_clusters, iter=2, thresh=tol / ratio)
-inertia1 = _labels_inertia(X, (X ** 2).sum(axis=-1), sc)[1]
-print('scipy inertia: %.1f' % inertia1)
-
-# Unfair comparison with kmeans2 (no way to specify a tolerance yet)
 ## print("\n-- scipy.cluster.vq")
-## sc, _ = utils.timeit(kmeans2)(X, n_clusters, iter=50)
-## print('scipy inertia: %.1f' % _labels_inertia(X, (X ** 2).sum(axis=-1), sc)[1])
+## ratio = 1.
+## np.random.seed(random_state)
+## sc, _ = utils.timeit(profile(kmeans))(X, n_clusters, iter=2,
+##                                       thresh=tol / ratio)
+## ## utils.cache_value(sc, 'prof_kmeans/scipy_kmeans_%d_%d'
+## ##                   % (n_samples, n_features))
+## inertia1 = _labels_inertia(X, (X ** 2).sum(axis=-1), sc)[1]
+## print('scipy inertia: %.1f' % np.sqrt(inertia1))
 
 print("\n-- sklearn.cluster")
-ratio = np.mean(np.var(X, axis=0))  # just to make the comparison fair.
-np.random.seed(1)
-sk, _, _ = utils.timeit(profile(k_means))(X, n_clusters, n_init=2, tol=tol / ratio,
-                                 init="random", random_state=1)
+ratio = 1. #np.mean(np.var(X, axis=0))  # just to make the comparison fair.
+
+np.random.seed(random_state)
+sk, _, _ = utils.timeit(profile(k_means))(X, n_clusters, n_init=2,
+                                          tol=tol / ratio,
+                                          init="random",
+                                          random_state=random_state)
+## utils.cache_value(sk, 'prof_kmeans/sklearn_kmeans_%d_%d' %
+##                   (n_samples, n_features))
 inertia2 = _labels_inertia(X, (X ** 2).sum(axis=-1), sk)[1]
-print('inertia: %.1f' % inertia2)
+print('inertia: %.1f' % np.sqrt(inertia2))
 
 ## print ('\nsklearn - scipy inertia: %.1f. Relative variation: %.1e' %
 ##        ((inertia2 - inertia1), (inertia2 - inertia1) / (
 ##            2. * (inertia1 + inertia2))))
-
-## print("\n-- sklearn minibatch")
-## mbk = utils.timeit(MiniBatchKMeans(n_clusters=n_clusters,
-##                                    n_init=2, tol=tol).fit)(X)
-## print('inertia: %.1f' % _labels_inertia(X, (X ** 2).sum(axis=-1),
-##                                         mbk.cluster_centers_)[1])
--- prof_pairwise_distance.py
+++ prof_pairwise_distance.py
@@ -1,5 +1,6 @@
 """Profiling of distance minimum distance computation"""
 import utils  # define profile() when not available
+import time
 
 import sys
 import numpy as np
@@ -27,22 +28,50 @@ def original(X, Y=None, axis=1, metric='euclidean', **kwargs):
 
 
 def chunked(X, Y=None, axis=1, metric="euclidean",
-            n_batches_x=None, chunk_y_num=None, **kwargs):
+            batch_size_x=None, batch_size_y=None, **kwargs):
     """Return argmin on the selected axis.
     axis 0 is along X
     axis 1 is along Y
     """
     out = pairwise_distances_argmin(X, Y=Y, axis=axis,
-                                    n_batches_x=n_batches_x,
-                                    chunk_y_num=chunk_y_num,
-                                    metric=metric, return_distances=True,
-                                    **kwargs)
+                                    batch_size_x=batch_size_x,
+                                    batch_size_y=batch_size_y,
+                                    metric=metric, **kwargs)
     return out
 
 
+def chunked_timing(X, Y=None, axis=1, metric="euclidean", **kwargs):
+    sizes = [20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+
+    t0 = time.time()
+    original(X, Y=None, axis=axis, metric="euclidean", **kwargs)
+    t1 = time.time()
+    original_timing = t1 - t0
+
+    chunked_timings = []
+
+    for batch_size in sizes:
+        print("batch_size: %d" % batch_size)
+        t0 = time.time()
+        chunked(X, Y=None, axis=axis, metric="euclidean",
+                batch_size_x=batch_size, batch_size_y=batch_size, **kwargs)
+        t1 = time.time()
+        chunked_timings.append(t1 - t0)
+
+    import pylab as pl
+    pl.semilogx(sizes, chunked_timings, '-+', label="chunked")
+    pl.hlines(original_timing, sizes[0], sizes[-1],
+              color='k', label="original")
+    pl.grid()
+    pl.xlabel("batch size")
+    pl.ylabel("execution time (wall clock)")
+    pl.title("%d / %d" % (X.shape[0], Y.shape[0]))
+    pl.legend()
+    pl.show()
+
 if __name__ == "__main__":
     np.random.seed(1)
-#    x_size, y_size = 200000, 50
+#    x_size, y_size = 10000, 50
 #    x_size, y_size = 100000, 500
 #    x_size, y_size = 10000, 5000
 #    x_size, y_size = 10000, 10000
@@ -51,8 +80,8 @@ if __name__ == "__main__":
 #    x_size, y_size = 50, 20
 #    x_size, y_size = 500, 200
 #    x_size, y_size = 500, 2000
-#    x_size, y_size = 5000, 2000
-    x_size, y_size = 2000, 5000
+    x_size, y_size = 5000, 2000
+#    x_size, y_size = 2000, 5000
 
     n_var = 200
     metric = "euclidean"
@@ -62,14 +91,17 @@ if __name__ == "__main__":
     X = np.random.rand(x_size, n_var)
     Y = np.random.rand(y_size, n_var)
     axis = 0
-    n_batches_x = 50
-    n_batches_y = 50
+    batch_size_x = 500
+    batch_size_y = 500
+
+#    chunked_timing(X, Y, metric=metric, axis=axis, **kwargs)
+#    sys.exit(0)
 
     dist_orig_ind, dist_orig_val = utils.timeit(profile(original))(
         X, Y, metric=metric, axis=axis, **kwargs)
     dist_chunked_ind, dist_chunked_val = utils.timeit(profile(chunked))(
         X, Y, axis=axis, metric=metric,
-        n_batches_x=n_batches_x, n_batches_y=n_batches_y, **kwargs)
+        batch_size_x=batch_size_x, batch_size_y=batch_size_y, **kwargs)
 
     np.testing.assert_almost_equal(dist_orig_ind, dist_chunked_ind, decimal=7)
     np.testing.assert_almost_equal(dist_orig_val, dist_chunked_val, decimal=7)
