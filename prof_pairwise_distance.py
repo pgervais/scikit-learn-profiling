d0c0f13e4cfe29010096a2df1fbbb61322f920c1
Philippe Gervais <philippe.gervais@inria.fr>
2013-08-27 13:53:38 +0200

    Updated prof_pairwise_distance
    Some modifications in scikit-learn have been made, the profiling
    code has been adapted.

--- prof_pairwise_distance.py
+++ prof_pairwise_distance.py
@@ -1,14 +1,14 @@
 """Profiling of distance minimum distance computation"""
 import utils  # define profile() when not available
+import sys
 import time
 
-import sys
 import numpy as np
 import scipy.spatial.distance as ssd
 
 from sklearn.metrics import euclidean_distances
 from sklearn.metrics import pairwise_distances
-from sklearn.metrics import pairwise_distances_argmin
+from sklearn.metrics import pairwise_distances_argmin_min
 
 
 def original(X, Y=None, axis=1, metric='euclidean', **kwargs):
@@ -27,24 +27,25 @@ def original(X, Y=None, axis=1, metric='euclidean', **kwargs):
         return indices, dist[indices, range(len(indices))]
 
 
-def chunked(X, Y=None, axis=1, metric="euclidean",
-            batch_size_x=None, batch_size_y=None, **kwargs):
+def chunked(X, Y, axis=1, metric="euclidean", batch_size=500, **kwargs):
     """Return argmin on the selected axis.
     axis 0 is along X
     axis 1 is along Y
     """
-    out = pairwise_distances_argmin(X, Y=Y, axis=axis,
-                                    batch_size_x=batch_size_x,
-                                    batch_size_y=batch_size_y,
-                                    metric=metric, **kwargs)
-    return out
+    return pairwise_distances_argmin_min(X, Y, axis=axis,
+                                         batch_size=batch_size,
+                                         metric=metric, **kwargs)
 
 
-def chunked_timing(X, Y=None, axis=1, metric="euclidean", **kwargs):
-    sizes = [20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+def chunked_timing(X, Y, axis=1, metric="euclidean", **kwargs):
+    sizes = [20, 50, 100,
+             200, 500, 1000,
+             2000, 5000, 10000,
+             20000, 50000, 100000,
+             200000]
 
     t0 = time.time()
-    original(X, Y=None, axis=axis, metric="euclidean", **kwargs)
+    original(X, Y, axis=axis, metric=metric, **kwargs)
     t1 = time.time()
     original_timing = t1 - t0
 
@@ -53,8 +54,8 @@ def chunked_timing(X, Y=None, axis=1, metric="euclidean", **kwargs):
     for batch_size in sizes:
         print("batch_size: %d" % batch_size)
         t0 = time.time()
-        chunked(X, Y=None, axis=axis, metric="euclidean",
-                batch_size_x=batch_size, batch_size_y=batch_size, **kwargs)
+        chunked(X, Y, axis=axis, metric=metric, batch_size=batch_size,
+                **kwargs)
         t1 = time.time()
         chunked_timings.append(t1 - t0)
 
@@ -65,8 +66,10 @@ def chunked_timing(X, Y=None, axis=1, metric="euclidean", **kwargs):
     pl.grid()
     pl.xlabel("batch size")
     pl.ylabel("execution time (wall clock)")
-    pl.title("%d / %d" % (X.shape[0], Y.shape[0]))
+    pl.title("%s %d / %d (axis %d)" % (
+        str(metric), X.shape[0], Y.shape[0], axis))
     pl.legend()
+    pl.savefig("%s_%d_%d_%d" % (str(metric), X.shape[0], Y.shape[0], axis))
     pl.show()
 
 if __name__ == "__main__":
@@ -77,31 +80,40 @@ if __name__ == "__main__":
 #    x_size, y_size = 10000, 10000
 #    x_size, y_size = 1000, 50000
 #    x_size, y_size = 50, 200000
+    x_size, y_size = 200000, 50
 #    x_size, y_size = 50, 20
 #    x_size, y_size = 500, 200
 #    x_size, y_size = 500, 2000
-    x_size, y_size = 5000, 2000
+#    x_size, y_size = 5000, 2000
 #    x_size, y_size = 2000, 5000
+#    x_size, y_size = 1500, 50
+#    x_size, y_size = 50, 1500
 
     n_var = 200
     metric = "euclidean"
-    #    metric = 'manhattan'
-    #    metric = ssd.canberra
-    kwargs = {"squared": False}
+#    metric = "chebyshev"
+    # metric = "mahalanobis" # doesn't work
+    # metric = 'manhattan'
+    # metric = "minkowski"
+
+    if metric == "euclidean":
+        kwargs = {"squared": False}
+    elif metric == "minkowski":
+        kwargs = {"p": 2}
+    else:
+        kwargs = {}
     X = np.random.rand(x_size, n_var)
     Y = np.random.rand(y_size, n_var)
-    axis = 0
-    batch_size_x = 500
-    batch_size_y = 500
+    axis = 1
+    batch_size = 200
 
-#    chunked_timing(X, Y, metric=metric, axis=axis, **kwargs)
-#    sys.exit(0)
+#    chunked_timing(X, Y, metric=metric, axis=axis, **kwargs); sys.exit(0)
 
     dist_orig_ind, dist_orig_val = utils.timeit(profile(original))(
         X, Y, metric=metric, axis=axis, **kwargs)
     dist_chunked_ind, dist_chunked_val = utils.timeit(profile(chunked))(
         X, Y, axis=axis, metric=metric,
-        batch_size_x=batch_size_x, batch_size_y=batch_size_y, **kwargs)
+        batch_size=batch_size, **kwargs)
 
     np.testing.assert_almost_equal(dist_orig_ind, dist_chunked_ind, decimal=7)
     np.testing.assert_almost_equal(dist_orig_val, dist_chunked_val, decimal=7)
